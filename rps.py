8cfd5db62ef112a4df3e7bad31e8e2f9285e2ea0
bloramarrero <bloramarrero28@stsaviour.org>
2026-03-25 08:39:07 -0400

    rps

--- rps.py
+++ rps.py
@@ -6,6 +6,13 @@ def print_dramatic_text(text: str, delay=0.1):
         print(char, end='', flush=True)
         time.sleep(delay)
     print()
-
 if __name__ == '__main__':
     print_dramatic_text('welcome to rock, paper, scissors! 🗿📃✂️')
+
+options = ['rock', 'paper', 'scisssors']
+choice = random,randint (0, 2)
+
+computer = options[choice]
+
+
+
